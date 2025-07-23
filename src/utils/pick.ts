792edaf4228fe40b09764c23ec3d11247933ce6b
Isaac Vidrine <idvidrine@gmail.com>
2025-07-23 03:55:20 -0400

    style: return type

--- src/utils/pick.ts
+++ src/utils/pick.ts
@@ -1,11 +1,11 @@
 /**
  * Create an object composed of the picked object properties
- * @param {Object} object
+ * @param {Object} _object
  * @param {string[]} keys
  * @returns {Object}
  */
 
-const pick = (_object: object, keys: string[]) => {
+const pick = (_object: object, keys: string[]): object => {
   return keys.reduce((obj, key) => {
     if (_object && Object.prototype.hasOwnProperty.call(_object, key)) {
       obj[key as keyof typeof obj] = _object[key as keyof typeof obj];
