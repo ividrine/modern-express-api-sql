7d4f4904e4ce6ae4a3e27c52b1ad84736d96121b
Isaac Vidrine <idvidrine@gmail.com>
2025-07-23 03:50:14 -0400

    refactor: config updates

--- kysely.config.ts
+++ kysely.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig, getKnexTimestampPrefix } from "kysely-ctl";
-import { db } from "./src/infrastructure/pg.client";
+import { Pool } from "pg";
 
 export default defineConfig({
-  kysely: db,
+  dialect: "pg",
+  dialectConfig: {
+    pool: new Pool({ connectionString: process.env.DATABASE_URL })
+  },
   migrations: {
-    migrationFolder: "db/migrations",
+    migrationFolder: "kysely/migrations",
     getMigrationPrefix: getKnexTimestampPrefix
   }
+
   // seeds: {
-  //   seedFolder: "db/seeds",
+  //   seedFolder: "kysely/seeds",
   //   getSeedPrefix: getKnexTimestampPrefix
   // }
 });
