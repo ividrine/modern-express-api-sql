a2aa9b82c167f1d8a4071c2dac27d0e3d3b7fa87
Isaac Vidrine <idvidrine@gmail.com>
2025-07-31 12:49:10 -0400

    refactor: zod schema validation

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,80 +1,63 @@
-import Joi from "joi";
+import * as z from "zod";
 
-const envVarsSchema = Joi.object()
-  .keys({
-    NODE_ENV: Joi.string()
-      .valid("production", "development", "test")
-      .required(),
-    PORT: Joi.number().default(3000),
-    DATABASE_URL: Joi.string().required().description("DB url"),
-    VALKEY_URL: Joi.string().required().description("Valkey url"),
-    JWT_SECRET: Joi.string().required().description("JWT secret key"),
-    JWT_ISSUER: Joi.string().required().description("JWT issuer"),
-    JWT_AUDIENCE: Joi.string().required().description("JWT issuer"),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number()
-      .default(30)
-      .description("minutes after which access tokens expire"),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number()
-      .default(30)
-      .description("days after which refresh tokens expire"),
-    JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description("minutes after which reset password token expires"),
-    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
-      .default(10)
-      .description("minutes after which verify email token expires"),
-    SMTP_HOST: Joi.string().description("server that will send the emails"),
-    SMTP_PORT: Joi.number().description("port to connect to the email server"),
-    SMTP_USERNAME: Joi.string().description("username for email server"),
-    SMTP_PASSWORD: Joi.string().description("password for email server"),
-    EMAIL_FROM: Joi.string().description(
-      "the from field in the emails sent by the app"
-    ),
-    AWS_ACCESS_KEY_ID: Joi.string().description(
-      "access key id for aws gamelift"
-    ),
-    AWS_SECRET_ACCESS_KEY: Joi.string().description(
-      "secret access key for aws gamelift"
-    )
-  })
-  .unknown();
+const envVarsSchema = z.object({
+  NODE_ENV: z.enum(["production", "development", "test"]),
+  PORT: z.coerce.number().default(3000),
+  DATABASE_URL: z.string().describe("DB Url"),
+  VALKEY_URL: z.string().describe("Valkey url"),
+  JWT_SECRET: z.string().describe("JWT secret key"),
+  JWT_ISSUER: z.string().describe("JWT issuer"),
+  JWT_AUDIENCE: z.string().describe("JWT issuer"),
+  JWT_ACCESS_EXPIRATION_MINUTES: z.coerce
+    .number()
+    .default(30)
+    .describe("minutes after which access tokens expire"),
+  JWT_REFRESH_EXPIRATION_DAYS: z.coerce
+    .number()
+    .default(30)
+    .describe("days after which refresh tokens expire"),
+  JWT_RESET_PASSWORD_EXPIRATION_MINUTES: z.coerce
+    .number()
+    .default(10)
+    .describe("minutes after which reset password token expires"),
+  JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: z.coerce
+    .number()
+    .default(10)
+    .describe("minutes after which verify email token expires"),
+  SMTP_HOST: z.string().describe("server that will send the emails"),
+  SMTP_PORT: z.coerce.number().describe("port to connect to the email server"),
+  SMTP_USERNAME: z.string().describe("username for email server"),
+  SMTP_PASSWORD: z.string().describe("password for email server"),
+  EMAIL_FROM: z
+    .string()
+    .describe("the from field in the emails sent by the app")
+});
 
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
-  .validate(process.env);
-
-if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
-}
+const data = envVarsSchema.parse(process.env);
 
 export default {
-  env: envVars.NODE_ENV,
-  port: envVars.PORT,
-  db_url: envVars.DATABASE_URL,
-  valkey_url: envVars.VALKEY_URL,
+  env: data.NODE_ENV,
+  port: data.PORT,
+  db_url: data.DATABASE_URL,
+  valkey_url: data.VALKEY_URL,
   jwt: {
-    iss: envVars.JWT_ISSUER,
-    aud: envVars.JWT_AUDIENCE,
-    secret: envVars.JWT_SECRET,
-    accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
-    refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
-    resetPasswordExpirationMinutes:
-      envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
-    verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES
+    iss: data.JWT_ISSUER,
+    aud: data.JWT_AUDIENCE,
+    secret: data.JWT_SECRET,
+    accessExpirationMinutes: data.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: data.JWT_REFRESH_EXPIRATION_DAYS,
+    resetPasswordExpirationMinutes: data.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
+    verifyEmailExpirationMinutes: data.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES
   },
   email: {
     smtp: {
-      host: envVars.SMTP_HOST,
-      port: envVars.SMTP_PORT,
+      host: data.SMTP_HOST,
+      port: data.SMTP_PORT,
       auth: {
-        user: envVars.SMTP_USERNAME,
-        pass: envVars.SMTP_PASSWORD
+        user: data.SMTP_USERNAME,
+        pass: data.SMTP_PASSWORD
       }
     },
-    from: envVars.EMAIL_FROM
-  },
-  aws: {
-    access_key_id: envVars.AWS_ACCESS_KEY_ID,
-    secret_access_key: envVars.AWS_SECRET_ACCESS_KEY
+    from: data.EMAIL_FROM
   }
 };
