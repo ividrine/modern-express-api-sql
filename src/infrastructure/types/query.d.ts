88844912ea1bf11d3bf88072c13387153cdaa2d0
Isaac Vidrine <idvidrine@gmail.com>
2025-08-07 00:48:09 -0400

    chore: code documentation for query types

--- src/infrastructure/types/query.d.ts
+++ src/infrastructure/types/query.d.ts
@@ -1,17 +1,41 @@
 import type { Operator } from "kysely";
 
-// Map a single column’s value type to either a raw value or a Kysely operator object
-type FilterOperation<Value> = {
+/**
+ * Maps a kysely operator to a raw value.
+ *
+ * syntax - { <operator> : <value> }
+ * example -  { "=" : "user@email.com" }
+ */
+export type OperatorCondition<Value> = {
   [Op in Operator]?: Value;
 };
 
-export type QueryField<O> = {
-  [K in keyof O]?: O[K] | FilterOperation<O[K]>;
+/**
+ * Maps a key of type O to either a raw value for
+ * equality operations or to an 'OperatorCondition'.
+ *
+ * syntax - { <column1>: <value>, <column2>: { <operator> : <value> } }
+ * example - { country: "US", age: { ">=": 30 } }
+ */
+export type QueryExpression<O> = {
+  [K in keyof O]?: O[K] | OperatorCondition<O[K]>;
 };
 
-export type QueryObject<O> = QueryField<O> & {
-  $and?: QueryField<O>[];
-  $or?: QueryField<O>[];
+/**
+ * QueryObject<O> is a 'QueryField' with additional
+ * properties for specifying the logical operation used.
+ * It works similar to mongodb query language but for kysely/sql.
+ *
+ * https://www.mongodb.com/docs/manual/reference/operator/query/and/#mongodb-query-op.-and
+ * https://www.mongodb.com/docs/manual/reference/operator/query/or/#mongodb-query-op.-or
+ *
+ * syntax - { $or: [ <QueryExpression1>, <QueryExpression2> ], $and: [ <QueryExpression1>, <QueryExpression2> ] }
+ * example for a "Places" table - { $or: { location} }
+ *
+ */
+export type QueryObject<O> = QueryExpression<O> & {
+  $and?: QueryExpression<O>[];
+  $or?: QueryExpression<O>[];
 };
 
 export type QueryOptions = {
